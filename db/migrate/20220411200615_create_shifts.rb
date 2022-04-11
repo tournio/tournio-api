1d806948143f5839c8f179af130d8c99b2712540
Scott Stebleton <scott@stebleton.net>
2022-04-11 16:13:35 -0400

    create model and migration for Shift

---
+++ db/migrate/20220411200615_create_shifts.rb
@@ -0,0 +1,14 @@
+class CreateShifts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shifts do |t|
+      t.string :name, null: false
+      t.string :description, null: false
+      t.integer :capacity, null: false, default: 40
+      t.integer :desired, null: false, default: 0
+      t.integer :confirmed, null: false, default: 0
+      t.references :tournament, null: false
+
+      t.timestamps
+    end
+  end
+end
