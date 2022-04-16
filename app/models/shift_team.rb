d8248fc529b8cf93db0a6f089675416e4aab24ae
Scott Stebleton <scott@stebleton.net>
2022-04-16 15:18:19 -0400

    rename desired to requested

--- app/models/shift_team.rb
+++ app/models/shift_team.rb
@@ -10,21 +10,21 @@
 class ShiftTeam < ApplicationRecord
   include AASM
 
-  self.table_name = 'shifts_tables'
+  self.table_name = 'shifts_teams'
 
   belongs_to :shift
   belongs_to :team
 
   after_create do
-    shift.update(desired: shift.desired + 1)
+    shift.update(requested: shift.requested + 1)
   end
 
   aasm do
-    state :desired, initial: true
+    state :requested, initial: true
     state :confirmed
 
     event :confirm do
-      transitions from: :desired, to: :confirmed
+      transitions from: :requested, to: :confirmed
 
       after do
         shift.update(confirmed: shift.confirmed + 1)
