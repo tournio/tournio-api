a01db6c4dc5afb779ba53787815a14399b24477f
Scott Stebleton <scott@stebleton.net>
2022-04-19 18:09:28 -0400

    uh...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_02_173709) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_11_202331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -192,6 +192,32 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_02_173709) do
     t.datetime "last_sent_at", precision: nil, null: false
     t.integer "bowler_count", default: 0
     t.index ["tournament_id"], name: "index_registration_summary_sends_on_tournament_id"
+  end
+
+  create_table "shifts", force: :cascade do |t|
+    t.string "identifier", null: false
+    t.string "name", null: false
+    t.string "description", null: false
+    t.integer "display_order", default: 1, null: false
+    t.integer "capacity", default: 40, null: false
+    t.integer "requested", default: 0, null: false
+    t.integer "confirmed", default: 0, null: false
+    t.bigint "tournament_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["identifier"], name: "index_shifts_on_identifier", unique: true
+    t.index ["tournament_id"], name: "index_shifts_on_tournament_id"
+  end
+
+  create_table "shifts_teams", id: false, force: :cascade do |t|
+    t.bigint "shift_id", null: false
+    t.bigint "team_id", null: false
+    t.string "aasm_state", null: false
+    t.datetime "confirmed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["shift_id"], name: "index_shifts_teams_on_shift_id"
+    t.index ["team_id"], name: "index_shifts_teams_on_team_id"
   end
 
   create_table "teams", force: :cascade do |t|
