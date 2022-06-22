4622bbaefca31862a02019092201f579ebdf18ab
Scott Stebleton <scott@stebleton.net>
2022-06-21 23:26:27 -0400

    write n/a instead of null for last sign-in time

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -4,7 +4,7 @@ class UserBlueprint < Blueprinter::Base
   identifier :identifier
 
   fields :email, :role
-  field :last_sign_in_at, datetime_format: '%F %R'
+  field :last_sign_in_at, datetime_format: '%F %R', default: 'n/a'
 
   association :tournaments, blueprint: TournamentBlueprint
 end
