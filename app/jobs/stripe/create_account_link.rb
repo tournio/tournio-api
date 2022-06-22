2d001598b73ef1c97f15c680741ebcd9eb74757a
Scott Stebleton <scott@stebleton.net>
2022-06-22 18:02:33 -0400

    WIP: setting up job classes to create Stripe accounts and links

--- app/jobs/stripe/create_account_link.rb
+++ app/jobs/stripe/create_account_link.rb
@@ -1,2 +1,19 @@
 class CreateAccountLink
+  include Sidekiq::Job
+
+  def perform(stripe_account_identifier)
+    acct = StripeAccount.find(stripe_account_identifier)
+    result = Stripe::AccountLink.create(
+      account: acct.identifier,
+      refresh_url: 'TBD',
+      return_url: 'TBD',
+      type: 'account_onboarding',
+    )
+    acct.update(
+      link_url: result.url,
+      link_expires_at: Time.at(result.expires_at),
+    )
+  rescue ActiveRecord::NotFoundError
+    Rails.logger.warn "Stripe account not found, identifier: #{stripe_account_identifier}"
+  end
 end
