dd3691b9edf9d71c0aaf8f11d39970b5dec7e93e
Scott Stebleton <scott@stebleton.net>
2022-07-11 15:43:34 -0700

    supply a friendly 404 message

--- app/controllers/checkout_sessions_controller.rb
+++ app/controllers/checkout_sessions_controller.rb
@@ -3,6 +3,6 @@ class CheckoutSessionsController < ApplicationController
     sc = StripeCheckoutSession.find_by!(identifier: params[:identifier])
     render json: StripeCheckoutSessionBlueprint.render(sc), status: :ok
   rescue ActiveRecord::RecordNotFound
-    render json: {}, status: :not_found
+    render json: { error: 'Could not find a checkout session with that identifier' }, status: :not_found
   end
 end
