f0c3dd5fc6c28c872b374e93001d8612a814c783
Scott Stebleton <scott@stebleton.net>
2023-01-24 16:00:00 -0500

    fix handling of refund event data

--- app/jobs/stripe/charge_refunded.rb
+++ app/jobs/stripe/charge_refunded.rb
@@ -2,7 +2,7 @@ module Stripe
   class ChargeRefunded < EventHandler
 
     def handle_event
-      charge = event[:object]
+      charge = event[:data][:object]
       payment_intent_identifier = charge[:payment_intent]
       credit_ledger_entry = LedgerEntry.find_by!(identifier: payment_intent_identifier)
       bowler_id = credit_ledger_entry.bowler_id
