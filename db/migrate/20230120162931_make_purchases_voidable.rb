34d957c2d1966595eeb1afdb4d4897e07e06c559
Scott Stebleton <scott@stebleton.net>
2023-02-16 13:03:19 -0500

    Get migrations caught up from rebase

--- db/migrate/20230120162931_make_purchases_voidable.rb
+++ db/migrate/20230120162931_make_purchases_voidable.rb
@@ -1,6 +1,6 @@
 class MakePurchasesVoidable < ActiveRecord::Migration[7.0]
   def change
-    add_column :purchases, :voided_at, :datetime
-    add_column :purchases, :void_reason, :string
+    add_column :purchases, :voided_at, :datetime, if_not_exists: true
+    add_column :purchases, :void_reason, :string, if_not_exists: true
   end
 end
--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -1,5 +1,28 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: purchases
+#
+#  id                  :bigint           not null, primary key
+#  amount              :integer          default(0)
+#  identifier          :string           not null
+#  paid_at             :datetime
+#  void_reason         :string
+#  voided_at           :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  bowler_id           :bigint
+#  external_payment_id :bigint
+#  purchasable_item_id :bigint
+#
+# Indexes
+#
+#  index_purchases_on_bowler_id            (bowler_id)
+#  index_purchases_on_external_payment_id  (external_payment_id)
+#  index_purchases_on_identifier           (identifier)
+#  index_purchases_on_purchasable_item_id  (purchasable_item_id)
+#
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
