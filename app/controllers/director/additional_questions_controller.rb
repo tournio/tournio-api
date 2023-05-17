bd92051ba176645694a495417345b1da8c0a6a79
Scott Stebleton <scott@stebleton.net>
2023-05-17 19:36:54 -0400

    no more placeholder

--- app/controllers/director/additional_questions_controller.rb
+++ app/controllers/director/additional_questions_controller.rb
@@ -7,25 +7,25 @@ module Director
 
       authorize tournament, :update?
 
-      # contact = Contact.new(new_contact_params)
-      # contact.tournament = tournament
-      # if (contact.save)
-      #   render json: ContactBlueprint.render(contact), status: :created
-      # end
+      question = AdditionalQuestion.new(new_question_params)
+      question.tournament = tournament
+      if (question.save)
+        render json: AdditionalQuestionBlueprint.render(question), status: :created
+      end
     rescue ActiveRecord::RecordNotFound
       skip_authorization
       render json: {}, status: :not_found
     end
 
     def update
-      # contact = Contact.includes(:tournament).find_by_identifier!(params[:identifier])
-      # self.tournament = contact.tournament
-      #
-      # authorize tournament, :update?
-      #
-      # if contact.update(update_contact_params)
-      #   render json: ContactBlueprint.render(contact.reload), status: :ok
-      # end
+      question = Question.includes(:tournament).find_by_identifier!(params[:identifier])
+      self.tournament = question.tournament
+
+      authorize tournament, :update?
+
+      if question.update(update_question_params)
+        render json: QuestionBlueprint.render(question.reload), status: :ok
+      end
     rescue ActiveRecord::RecordNotFound
       skip_authorization
       render json: {}, status: :not_found
@@ -39,12 +39,12 @@ module Director
 
     attr_accessor :tournament, :question
 
-    # def new_question_params
-    #   params.permit(:tournament_identifier, additional_question: %i(name email role notify_on_payment notify_on_registration notification_preference)).require(:additional_question)
-    # end
-    #
-    # def update_question_params
-    #   params.require(:additional_question).permit(%i(name email role notify_on_payment notify_on_registration notification_preference))
-    # end
+    def new_question_params
+      params.permit(:tournament_identifier, additional_question: %i(extended_form_field_id, required)).require(:additional_question)
+    end
+
+    def update_question_params
+      params.require(:additional_question).permit(%i(required))
+    end
   end
 end
