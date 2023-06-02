f3e60dbdd1eacc75b36c95d449db75e38b2f8f86
Scott Stebleton <scott@stebleton.net>
2023-06-02 14:13:44 -0400

    properly fall back to director if no other contact scopes match

--- app/jobs/payment_receipt_notifier_job.rb
+++ app/jobs/payment_receipt_notifier_job.rb
@@ -82,7 +82,9 @@ class PaymentReceiptNotifierJob < TemplateMailerJob
 
   def treasurer_contact
     if Rails.env.production?
-      tournament.contacts.treasurer.first&.email || tournament.contacts.payment_notifiable.first&.email || tournament.contacts.director.first&.email
+      tournament.contacts.treasurer.first&.email ||
+        tournament.contacts.payment_notifiable.first&.email ||
+        tournament.contacts.director.first&.email
     else
       FROM_ADDRESS
     end
