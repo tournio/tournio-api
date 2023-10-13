24e859d1943ea7e93ab950f5376e08e85f295ee2
Scott Stebleton <scott@stebleton.net>
2023-10-13 15:40:27 -0400

    Add trait for including additional questions

--- spec/requests/bowlers_spec.rb
+++ spec/requests/bowlers_spec.rb
@@ -50,17 +50,7 @@ describe BowlersController, type: :request do
     subject { post uri, params: bowler_params, as: :json }
 
     let(:uri) { "/tournaments/#{tournament.identifier}/bowlers" }
-    let(:tournament) { create :tournament, :active, :with_entry_fee }
-
-    before do
-      comment = create(:extended_form_field, :comment)
-      pronouns = create(:extended_form_field, :pronouns)
-      standings = create(:extended_form_field, :standings_link)
-
-      create(:additional_question, extended_form_field: comment, tournament: tournament)
-      create(:additional_question, extended_form_field: pronouns, tournament: tournament)
-      create(:additional_question, extended_form_field: standings, tournament: tournament)
-    end
+    let(:tournament) { create :tournament, :active, :with_entry_fee, :with_additional_questions }
 
     context 'adding a bowler to a team' do
       let!(:team) { create :team, :standard_three_bowlers, tournament: tournament }
@@ -196,7 +186,7 @@ describe BowlersController, type: :request do
       end
 
       context 'a tournament with event selection' do
-        let(:tournament) { create :tournament, :active, :with_a_bowling_event }
+        let(:tournament) { create :tournament, :active, :with_a_bowling_event, :with_additional_questions }
         let(:bowler_params) do
           {
             bowlers: [
@@ -279,7 +269,7 @@ describe BowlersController, type: :request do
       end
 
       context 'a tournament with event selection' do
-        let(:tournament) { create :tournament, :active, :with_a_bowling_event }
+        let(:tournament) { create :tournament, :active, :with_a_bowling_event, :with_additional_questions }
         let(:bowler_params) do
           {
             bowlers: [
@@ -300,7 +290,7 @@ describe BowlersController, type: :request do
     end
 
     context 'registering as a doubles pair' do
-      let(:tournament) { create :tournament, :active, :with_a_bowling_event }
+      let(:tournament) { create :tournament, :active, :with_a_bowling_event, :with_additional_questions }
       let(:bowler_params) do
         {
           bowlers: create_doubles_test_data,
