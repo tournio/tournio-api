2b234a844c97d36e2280daf42af02b90d602fd69
Scott Stebleton <scott@stebleton.net>
2023-12-19 17:48:18 -0500

    Remove the discount expiration concept

--- spec/jobs/schedule_purchase_voids_job_spec.rb
+++ spec/jobs/schedule_purchase_voids_job_spec.rb
@@ -14,35 +14,33 @@ RSpec.describe SchedulePurchaseVoidsJob, type: :job do
         :one_shift,
         :with_entry_fee
     end
-    let(:discount_item) do
-      create :purchasable_item,
-        :early_discount,
-        tournament: tournament,
-        configuration: { valid_until: '2022-12-28T12:37:00-04:00' }
-    end
+    let(:item_to_void) { tournament.purchasable_items.entry_fee.first }
 
     let(:job_object) { described_class.new }
-    subject { job_object.perform(discount_item.id, why) }
+    subject { job_object.perform(item_to_void.id, why) }
 
     before do
       10.times do |i|
         b = create :bowler, tournament: tournament
-        create :purchase, purchasable_item: discount_item, bowler: b, amount: discount_item.value
+        create :purchase,
+          purchasable_item: item_to_void,
+          bowler: b,
+          amount: item_to_void.value
       end
       5.times do |i|
         b = create :bowler, tournament: tournament
         create :purchase,
-          purchasable_item: discount_item,
+          purchasable_item: item_to_void,
           bowler: b,
-          amount: discount_item.value,
+          amount: item_to_void.value,
           paid_at: wall_clock_time.advance(days: -6)
       end
       3.times do |i|
         b = create :bowler, tournament: tournament
         create :purchase,
-          purchasable_item: discount_item,
+          purchasable_item: item_to_void,
           bowler: b,
-          amount: discount_item.value,
+          amount: item_to_void.value,
           voided_at: wall_clock_time.advance(days: -6)
       end
     end
