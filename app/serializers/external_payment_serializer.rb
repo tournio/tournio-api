13b948352b5d2b5cf966cc703e26ed6537b35b22
Scott Stebleton <scott@stebleton.net>
2024-01-05 12:29:11 -0500

    annotation updates

--- app/serializers/external_payment_serializer.rb
+++ app/serializers/external_payment_serializer.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: external_payments
+#
+#  id            :bigint           not null, primary key
 #  details       :jsonb
 #  identifier    :string
 #  payment_type  :integer          not null
 #  created_at    :datetime         not null
 #  updated_at    :datetime         not null
 #  tournament_id :bigint
+#
+# Indexes
+#
+#  index_external_payments_on_identifier     (identifier)
+#  index_external_payments_on_tournament_id  (tournament_id)
+#
 
 class ExternalPaymentSerializer < JsonSerializer
   attributes :identifier
