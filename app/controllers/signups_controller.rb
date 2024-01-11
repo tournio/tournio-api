08fe9a0954d61dc89af7e48b2016fbc0cdb82bc0
Scott Stebleton <scott@stebleton.net>
2024-01-11 05:58:32 -0500

    signups#update controller action and request spec

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,16 +1,26 @@
 class SignupsController < ApplicationController
   wrap_parameters false
+
   before_action :load_bowler
   before_action :load_signup
 
   PERMITTED_EVENTS = %w(request never_mind pay)
 
-  class UnpermittedEventError < ::RuntimeError
+  class IllegalEventError < ::RuntimeError
   end
 
   def update
+    unless bowler.present? && signup.present?
+      render json: nil, status: :not_found
+      return
+    end
+
     signup.send(convert_event_name)
-  rescue UnpermittedEventError => e
+
+    render json: SignupSerializer.new(signup).as_json, status: :ok
+  rescue AASM::InvalidTransition => e
+    render json: nil, status: :conflict
+  rescue IllegalEventError => e
     render json: nil, status: :unprocessable_entity
   end
 
@@ -25,15 +35,17 @@ class SignupsController < ApplicationController
   end
 
   def load_signup
-    @signup = bowler.signups.find_by_identifier(signup_params[:identifier])
+    if bowler.present?
+      @signup = bowler.signups.find_by_identifier(signup_params[:identifier])
+    end
   end
 
   def signup_params
-    @signup_params ||= params.require(%i(identifier event))
+    @signup_params ||= params.permit(%i(identifier event))
   end
 
   def convert_event_name
-    raise UnpermittedEventError unless PERMITTED_EVENTS.include? signup_params[:event]
+    raise IllegalEventError unless PERMITTED_EVENTS.include? signup_params[:event]
     "#{signup_params[:event]}!".to_sym
   end
 end
