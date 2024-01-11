fc4f9f7b9f5f0844dd3e510a5b706daf62c07273
Scott Stebleton <scott@stebleton.net>
2024-01-11 05:58:32 -0500

    signups#update controller action and request spec

--- spec/requests/signups_spec.rb
+++ spec/requests/signups_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe SignupsController, type: :request do
+describe SignupsController, type: :request do
   let(:headers) do
     {
       'Accept': 'application/json',
@@ -8,19 +8,22 @@ RSpec.describe SignupsController, type: :request do
     }
   end
 
-  describe "PATCH :update" do
-    subject { patch uri, headers: auth_headers, params: params, as: :json }
+  describe "#update" do
+    subject { patch uri, params: params, as: :json }
 
-    let(:uri) { "/#{signup_identifier}" }
+    let(:uri) { "/signups/#{signup_identifier}" }
 
-    let(:bowler) { create :bowler, :with_team }
+    let(:tournament) { create :tournament, :active, :with_an_optional_event }
+    let(:bowler) do
+      create :bowler,
+        tournament: tournament
+    end
     let(:bowler_identifier) { bowler.identifier }
-    let(:tournament) { bowler.tournament }
     let(:purchasable_item) do
       create :purchasable_item, :optional_event,
         tournament: tournament
     end
-    let(:signup) { Signup.create(bowler: bowler, purchasable_item: purchasable_item) }
+    let(:signup) { create :signup, bowler: bowler, purchasable_item: purchasable_item }
     let(:signup_identifier) { signup.identifier }
 
     let(:params) do
@@ -34,28 +37,32 @@ RSpec.describe SignupsController, type: :request do
     let(:event_name) { 'request' }
 
     it 'returns a 200 OK' do
+      subject
       expect(response).to have_http_status(:ok)
     end
 
     it 'returns an updated Signup with in the response' do
+      subject
       expect(json['status']).to eq('requested')
     end
 
     context 'changing their mind after requesting' do
+      let(:signup) { create :signup, :requested, bowler: bowler, purchasable_item: purchasable_item }
       let(:event_name) { 'never_mind' }
 
-      expect(json['status']).to eq('initial')
+      it 'returns the status to initial' do
+        subject
+        expect(json['status']).to eq('initial')
+      end
     end
 
     describe 'error scenarios' do
       context 'changing their mind after paying' do
+        let(:signup) { create :signup, :paid, bowler: bowler, purchasable_item: purchasable_item }
         let(:event_name) { 'never_mind' }
 
-        before do
-          signup.pay!
-        end
-
         it 'rejects the request with Conflict' do
+          subject
           expect(response).to have_http_status(:conflict)
         end
       end
@@ -64,6 +71,7 @@ RSpec.describe SignupsController, type: :request do
         let(:event_name) { 'lolwut' }
 
         it 'rejects the request with Unprocessable Entity' do
+          subject
           expect(response).to have_http_status(:unprocessable_entity)
         end
       end
@@ -72,6 +80,7 @@ RSpec.describe SignupsController, type: :request do
         let(:bowler_identifier) { 'i-dont-know-her' }
 
         it 'rejects the request with Unprocessable Not Found' do
+          subject
           expect(response).to have_http_status(:not_found)
         end
       end
@@ -80,10 +89,10 @@ RSpec.describe SignupsController, type: :request do
         let(:signup_identifier) { 'my-totally-legit-signup' }
 
         it 'rejects the request with Unprocessable Not Found' do
+          subject
           expect(response).to have_http_status(:not_found)
         end
       end
     end
   end
-
 end
