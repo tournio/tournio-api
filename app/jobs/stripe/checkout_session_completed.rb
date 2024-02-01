49c18fb6dfc43ba410fac5876e2a0c974c1c02f0
Scott Stebleton <scott@stebleton.net>
2024-02-01 12:51:39 -0500

    deactivate other signups in a division when one is paid

--- app/jobs/stripe/checkout_session_completed.rb
+++ app/jobs/stripe/checkout_session_completed.rb
@@ -56,9 +56,18 @@ module Stripe
         end
 
         # mark any related Signups as paid
-        signup = bowler.signups.find_by(purchasable_item_id: pi.id)
+        signup = bowler.signups.find_by_purchasable_item_id(pi.id)
         if signup.present?
           signup.pay!
+
+          # if it's a division item, disable the rest
+          if pi.division?
+            bowler.tournament.purchasable_items.division.where(name: pi.name).map do |div_pi|
+              unless div_pi.id == pi.id
+                bowler.signups.find_by_purchasable_item_id(div_pi.id).deactivate!
+              end
+            end
+          end
         end
 
         # any discounts to apply, e.g., bundle discount for just-purchased events?
