764e950c7873194caa44fdfd4699c769c9f5eef8
Scott Stebleton <scott@stebleton.net>
2024-08-14 11:30:23 -0400

    allow a manual payment amount to cover a late fee in addition to entry fee

--- app/controllers/director/ledger_entries_controller.rb
+++ app/controllers/director/ledger_entries_controller.rb
@@ -4,6 +4,7 @@ module Director
 
     def create
       self.bowler = Bowler.includes(:tournament).find_by_identifier!(params[:bowler_identifier])
+      self.tournament = bowler.tournament
 
       authorize bowler, :update?
 
@@ -17,13 +18,13 @@ module Director
         payment_type: :manual,
         identifier: SecureRandom.uuid,
         details: entry.identifier,
-        tournament: bowler.tournament
+        tournament: tournament
       )
 
       # Assume the payment is for the entry fee only, until we let directors specify what a manual payment is for
       # (The idea there is to allow arbitrary amounts, rather than assuming it's for the entry fee)
       if bowler.purchases.entry_fee.empty?
-        entry_fee_item = bowler.tournament.purchasable_items.entry_fee.first
+        entry_fee_item = tournament.purchasable_items.entry_fee.first
         bowler.purchases << Purchase.new(
           purchasable_item: entry_fee_item,
           amount: entry_fee_item.value,
@@ -31,6 +32,22 @@ module Director
           external_payment_id: extp.id
         )
       end
+
+      # If the manual amount also covers a late fee, let's add that.
+      late_fee_item = tournament.purchasable_items.late_fee.first
+      if late_fee_item.present?
+        remainder = entry.credit - entry_fee_item.value
+
+        if remainder == late_fee_item.value
+          bowler.purchases << Purchase.new(
+            purchasable_item: late_fee_item,
+            amount: late_fee_item.value,
+            paid_at: Time.zone.now,
+            external_payment_id: extp.id
+          )
+        end
+      end
+
 
       render json: LedgerEntryBlueprint.render(entry), status: :created
     rescue ActiveRecord::RecordNotFound
@@ -40,7 +57,7 @@ module Director
 
     private
 
-    attr_accessor :bowler, :ledger_entry
+    attr_accessor :bowler, :ledger_entry, :tournament
 
     def new_entry_params
       params.permit(:bowler_identifier, ledger_entry: [:credit, :identifier]).require(:ledger_entry)
